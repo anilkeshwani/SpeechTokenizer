a96658e6c66cfa0cb30f83dbc5bf2ba815d4f7bd
anilkeshwani <anilkeshwani@hotmail.com>
2025-04-07 15:42:14 +0000

    Feature extraction with HuBERT works
    Need to pass *nested* HF model dir path to semantic_model_path in cfg

--- scripts/hubert_rep_extract.sh
+++ scripts/hubert_rep_extract.sh
@@ -1,16 +1,13 @@
 CONFIG="config/spt_base_cfg.json"
-AUDIO_DIR="/remote-home/share/data/SpeechPretrain/LibriSpeech/LibriSpeech"
-REP_DIR="/remote-home/share/data/SpeechPretrain/hubert_rep/LibriSpeech"
+AUDIO_DIR="/mnt/scratch-artemis/anilkeshwani/tmp/st/mls-train-audio"
+REP_DIR="/mnt/scratch-artemis/anilkeshwani/tmp/st/mls-train-reps"
 EXTS="flac"
 SPLIT_SEED=0
-VALID_SET_SIZE=1500
+VALID_SET_SIZE=1
 
-
-
-CUDA_VISIBLE_DEVICES=0 python scripts/hubert_rep_extract.py\
-    --config ${CONFIG}\
-    --audio_dir ${AUDIO_DIR}\
-    --rep_dir ${REP_DIR}\
-    --exts ${EXTS}\
-    --split_seed ${SPLIT_SEED}\
+CUDA_VISIBLE_DEVICES=0 python scripts/hubert_rep_extract.py --config ${CONFIG} \
+    --audio_dir ${AUDIO_DIR} \
+    --rep_dir ${REP_DIR} \
+    --exts ${EXTS} \
+    --split_seed ${SPLIT_SEED} \
     --valid_set_size ${VALID_SET_SIZE}
